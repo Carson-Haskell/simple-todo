d11f0081a538a43d5392722a1a62330bb9d4c4be
Carson Haskell <carson.haskell@gmail.com>
2023-05-04 20:07:54 -0700

    remove unused useState import in TodoProvider

--- src/TodoProvider.jsx
+++ src/TodoProvider.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer, createContext, useEffect } from "react";
+import { useReducer, createContext, useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 export const TodoContext = createContext();
